3a10042a9691e9ee182c3e28824a671604e2b7d6
OheneKwabena <adjeiasarejesse@gmail.com>
2023-02-18 23:47:17 +0000

    Take user input and write function to play single round of game

--- functions.js
+++ functions.js
@@ -13,4 +13,51 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-console.log(getComputerChoice());
+let playerSelection = prompt("What do you choose? Rock, Paper or Scissors?");
+
+// console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  let A = playerSelection;
+  let B = computerSelection;
+
+  A = A.toLowerCase();
+  B = B.toLowerCase();
+
+  //   if (A === "rock" && B === "scissors") {
+  //     return "You win! Rock beats Scissors";
+  //   } else if (A === "rock" && B === "paper") {
+  //     return "You lose! Paper beats Rock";
+  //   } else if (A === "paper" && B === "rock") {
+  //     return "You win! Paper beats Rock";
+  //   }
+
+  switch (true) {
+    case A === "rock" && B === "paper":
+      return "You lose! Paper beats rock";
+      break;
+
+    case A === "rock" && B === "scissors":
+      return "You win! Rock beats Scissors";
+      break;
+
+    case A === "paper" && B === "rock":
+      return "You win! Paper beats rock";
+      break;
+
+    case A === "paper" && B === "scissors":
+      return "You lose! Scissors beats Paper";
+      break;
+
+    case A === "scissors" && B === "rock":
+      return "You lose! Rock beats Scissors";
+      break;
+
+    case A === "scissors" && B === "paper":
+      return "You win! Scissors beats Paper";
+      break;
+
+    default:
+      return "That's a draw!";
+  }
+}
